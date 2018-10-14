55ab84aeec26d46a8de8f6a0ea451ef5d8acdaea
EC2 Default User <ec2-user@ip-172-31-86-190.ec2.internal>
2018-10-14 14:01:37 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -49,6 +49,10 @@ class TasksController extends Controller
      // postでtasks/にアクセスされた場合の「新規登録処理」
     public function store(Request $request) //creteで入力されたデータは「$request」に入っている
     {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]); //required (カラッポでない) かつ max:191 を検証
+        
         $task =new Task;
         $task->content =$request->content; //creteで入力された内容を「$task」へ代入
         $task->save();
@@ -102,6 +106,10 @@ class TasksController extends Controller
      // putまたはpatchでtasks/idにアクセスされた場合の「更新処理」
     public function update(Request $request, $id)//editで入力されたデータは「$request」に入っている
     {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]); //required (カラッポでない) かつ max:191 を検証
+        
         $task = Task::find($id);//$idを探して$taskに代入
         $task->content =$request->content; //editで入力された内容を「$task」へ代入
         $task->save();
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,9 +7,11 @@
   @if(count($tasks)>0) <!--$tasksレコードが1個以上あれば-->
     <ul>
         @foreach($tasks as $task)<!--一つづつ取り出して表示する-->
-          <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li><!--「show」へのリンク-->
+          <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} <!--id表示と「show」へのリンク-->
+          : {{ $task->content }}</li>
         @endforeach
     </ul>
   @endif
+    {!! link_to_route('tasks.create', '新規タスクの投稿') !!}
 
 @endsection
